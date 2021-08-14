ce6bac534660180f0c1d06487dfac3b75819d8aa
Idiot <verruckterdrachen@gmail.com>
2021-08-14 11:55:19 +0300

    add welcome section

--- #src/js/files/sliders.js
+++ #src/js/files/sliders.js
@@ -61,63 +61,35 @@ if (sliderScrollItems.length > 0) {
 
 function sliders_bild_callback(params) { }
 
-let slider_about = new Swiper('.about__slider', {
-	/*
-	effect: 'fade',
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-	},
-	*/
-	observer: true,
-	observeParents: true,
-	slidesPerView: 1,
-	spaceBetween: 0,
-	autoHeight: true,
-	speed: 800,
-	//touchRatio: 0,
-	//simulateTouch: false,
-	//loop: true,
-	//preloadImages: false,
-	//lazy: true,
-	// Dotts
-	//pagination: {
-	//	el: '.slider-quality__pagging',
-	//	clickable: true,
-	//},
-	// Arrows
-	navigation: {
-		nextEl: '.about__more .more__item_next',
-		prevEl: '.about__more .more__item_prev',
-	},
-	/*
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			autoHeight: true,
+if (document.querySelector('.slider-welcome__body')) {
+	let welcomeSlider = new Swiper('.slider-welcome__body', {
+		observer: true,
+		observeParents: true,
+		autoHeight: false,
+		parallax: true,
+		loop: true,
+		pagination: {
+			el: '.slider-welcome__dotts',
+			clickable: true,
 		},
-		768: {
-			slidesPerView: 2,
-			spaceBetween: 20,
+		autoplay: {
+			delay: 2500,
+			stopOnLastSlide: false,
+			disableOnInteraction: false
 		},
-		992: {
-			slidesPerView: 3,
-			spaceBetween: 20,
-		},
-		1268: {
-			slidesPerView: 4,
-			spaceBetween: 30,
-		},
-	},
-	*/
-	on: {
-		lazyImageReady: function () {
-			ibg();
-		},
-	}
-	// And if we need scrollbar
-	//scrollbar: {
-	//	el: '.swiper-scrollbar',
-	//},
-});
+		speed: 1500,
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 20,
+			},
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 120,
+			}
+		}
+	});
+}
